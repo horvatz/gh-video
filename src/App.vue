78b86f74c960545c855c3ab1e6d5e094705e6d56
Zan Horvat <zanhorvat8@gmail.com>
2020-09-27 19:02:31 +0200

    Dodane slike, popravki galerije in naslovnih vrstic

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,51 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-      app
-    >
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="title">
-          Geza Horvat Video
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          Popotniški filmi
-        </v-list-item-subtitle>
+    <v-navigation-drawer v-model="drawer" temporary app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Geza Horvat Video
+          </v-list-item-title>
+          <v-list-item-subtitle> Popotniški filmi </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
-      <v-list
-        nav
-        dense
-      >
-          <v-list-item link to="/">
+      <v-list nav dense>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-flag</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-icon>
-              <v-icon>mdi-flag</v-icon>
-            </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Države</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-            <v-list-item-content>
-              <v-list-item-title>Države</v-list-item-title>
-            </v-list-item-content>
-            
-          </v-list-item>
+        <v-list-item link to="/gallery">
+          <v-list-item-icon>
+            <v-icon>mdi-image</v-icon>
+          </v-list-item-icon>
 
-          <v-list-item link to="/gallery">
-            
-            <v-list-item-icon>
-              <v-icon>mdi-image</v-icon>
-            </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Umetniške slike</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-            <v-list-item-content>
-              <v-list-item-title>Umetniške slike</v-list-item-title>
-            </v-list-item-content>
-            
-          </v-list-item>
+        <v-list-item link to="about">
+          <v-list-item-icon>
+            <v-icon>mdi-help</v-icon>
+          </v-list-item-icon>
 
-          <v-list-item link to="about">
-            
-            <v-list-item-icon>
-              <v-icon>mdi-help</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>O strani</v-list-item-title>
-            </v-list-item-content>
-            
-          </v-list-item>
-
+          <v-list-item-content>
+            <v-list-item-title>O strani</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      color="blue darken-4"
-      dark
-      app
-    >
+    <v-app-bar color="blue darken-4" dark app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <router-link to="/" class="text-decoration-none white--text">
-        <v-toolbar-title >Geza Horvat Video</v-toolbar-title>
+        <v-toolbar-title>Geza Horvat Video</v-toolbar-title>
       </router-link>
 
       <v-spacer></v-spacer>
@@ -82,60 +62,47 @@
         <v-icon>mdi-help</v-icon>
       </v-btn>
     </v-app-bar>
-   
+
     <!-- Sizes your content based upon application components -->
     <v-main>
-
       <!-- Provides the application the proper gutter -->
-      
 
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      
+      <!-- If using vue-router -->
+      <router-view></router-view>
     </v-main>
 
-    
-    <v-card height="150" >
-      <v-footer
-        absolute
-        class="font-weight-medium" color="blue darken-4" dark
-      >
-        <v-col
-          class="text-center"
-          cols="12"
-        >
-        {{ new Date().getFullYear() }} — <strong>gezahorvatvideo.com</strong>
+    <v-card height="150">
+      <v-footer absolute class="font-weight-medium" color="blue darken-4" dark>
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>gezahorvatvideo.com</strong>
         </v-col>
-        <cookie-law v-on:accept="$router.push({ name: 'Home' })" buttonText="Sprejmi">
+        <cookie-law
+          v-on:accept="$router.push({ name: 'Home' })"
+          buttonText="Sprejmi"
+        >
           <div slot="message">
-            Za boljše delovanje spletna stran uporablja piškotke. <router-link to="/legal">Več informacij</router-link>
+            Za boljše delovanje spletna stran uporablja piškotke.
+            <router-link to="/legal">Več informacij</router-link>
           </div>
         </cookie-law>
       </v-footer>
     </v-card>
-
   </v-app>
 </template>
 
 <script>
-import CookieLaw from 'vue-cookie-law'
+import CookieLaw from "vue-cookie-law";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CookieLaw
+    CookieLaw,
   },
 
   data: () => ({
-    drawer: null
+    drawer: null,
   }),
-  watch: {
-    '$route' (to) {
-      document.title = to.meta.title || 'GezaHorvatVideo'
-    }
-  },
 };
 </script>
 
 <style scoped>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,78 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import CountryShow from '../views/CountryShow.vue'
-import VideoShow from '../views/VideoShow.vue'
-import Cookies from '../views/Cookies.vue'
-import Gallery from '../views/Gallery.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import CountryShow from "../views/CountryShow.vue";
+import VideoShow from "../views/VideoShow.vue";
+import Cookies from "../views/Cookies.vue";
+import Gallery from "../views/Gallery.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      title: "Domov | GezaHorvatVideo"
-    }
-  },
-  {
-    path: '/drzava/:countryName',
-    name: 'country-show',
-    component: CountryShow,
-    props: true,
-    meta: {
-      title: "Videposnetki | GezaHorvatVideo"
-    }
-  },
-  {
-    path: '/video/:id',
-    name: 'video-show',
-    component: VideoShow,
-    props: true,
-    meta: {
-      title: "Videoposnetki | GezaHorvatVideo"
-    }
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    meta: {
-      title: "O strani | GezaHorvatVideo"
-    }
-  },
-  {
-    path: '/legal',
-    name: 'Legal',
-    component: Cookies,
-    meta: {
-      title: "Zasebnost | GezaHorvatVideo"
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+        meta: {
+            title: "Domov | GezaHorvatVideo",
+        },
     },
-  },
-  {
-    path: '/gallery',
-    name: 'Gallery',
-    component: Gallery,
-    meta: {
-      title: "Galerija | GezaHorvatVideo"
+    {
+        path: "/drzava/:countryName",
+        name: "country-show",
+        component: CountryShow,
+        props: true,
+        meta: {
+            title: "Videposnetki | GezaHorvatVideo",
+        },
     },
-  }
-]
+    {
+        path: "/video/:id",
+        name: "video-show",
+        component: VideoShow,
+        props: true,
+        meta: {
+            title: "Videoposnetki | GezaHorvatVideo",
+        },
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/About.vue"),
+        meta: {
+            title: "O strani | GezaHorvatVideo",
+        },
+    },
+    {
+        path: "/legal",
+        name: "Legal",
+        component: Cookies,
+        meta: {
+            title: "Zasebnost | GezaHorvatVideo",
+        },
+    },
+    {
+        path: "/gallery",
+        name: "Gallery",
+        component: Gallery,
+        meta: {
+            title: "Galerija | GezaHorvatVideo",
+        },
+    },
+];
 
 const router = new VueRouter({
-  routes,
-  mode: 'history'
-})
+    routes,
+    mode: "history",
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title;
+    next();
+});
+
+export default router;
