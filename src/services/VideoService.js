a2dd9b0f489372c5feb8ac7f87c2356ccc03d1c0
Žan Horvat <zanhorvat8@gmail.com>
2023-06-14 21:25:05 +0200

    ci: fix gh action

--- src/services/VideoService.js
+++ src/services/VideoService.js
@@ -1,35 +1,44 @@
-import axios from 'axios'
+import axios from "axios";
 
-const apiKey = process.env.VUE_APP_YOUTUBE_KEY
+const apiKey = "AIzaSyDsYheDDVfl-7j3Oa4Lt3pGol8D8s4_zo8";
 
 const apiClient = axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
 
 export default {
-    getCountry() {
-        return apiClient.get("/playlists?part=snippet%2CcontentDetails&channelId=UCaNAZAJLNhPypJIhgCHByiQ&maxResults=60&fields=items", {
-            params: {
-                key: apiKey
-            }
-        });
-    },
-    getVideoList(id) {
-        return apiClient.get(`/playlistItems?part=snippet%2CcontentDetails&maxResults=40&playlistId=${id}&fields=items`, {
-            params: {
-                key: apiKey
-            }
-        })
-    },
-    getVideoInfo(id) {
-        return apiClient.get(`/videos?part=snippet%2Cstatistics&id=${id}&fields=items`, {
-            params: {
-                key: apiKey
-            }
-        })
-    }
-}
+  getCountry() {
+    return apiClient.get(
+      "/playlists?part=snippet%2CcontentDetails&channelId=UCaNAZAJLNhPypJIhgCHByiQ&maxResults=60&fields=items",
+      {
+        params: {
+          key: apiKey,
+        },
+      }
+    );
+  },
+  getVideoList(id) {
+    return apiClient.get(
+      `/playlistItems?part=snippet%2CcontentDetails&maxResults=40&playlistId=${id}&fields=items`,
+      {
+        params: {
+          key: apiKey,
+        },
+      }
+    );
+  },
+  getVideoInfo(id) {
+    return apiClient.get(
+      `/videos?part=snippet%2Cstatistics&id=${id}&fields=items`,
+      {
+        params: {
+          key: apiKey,
+        },
+      }
+    );
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: "/gh-video/",
+  publicPath: "/",
   transpileDependencies: ["vuetify"],
 };
